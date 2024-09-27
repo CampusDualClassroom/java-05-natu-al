4aeed75fa7c7a0ababd5274dc69e886948c6f0e3
Nataly Alvarado Lujan <nataly.al.luj@gmail.com>
2024-09-27 11:27:14 +0200

    Resuelto

--- src/main/java/com/campusdual/classroom/Exercise05.java
+++ src/main/java/com/campusdual/classroom/Exercise05.java
@@ -2,27 +2,47 @@ package com.campusdual.classroom;
 
 public class Exercise05 {
     public static void main(String[] args) {
-
+        checkLessOrGreaterEqual(5, 9);
+        checkLessOrGreaterEqual(2,6);
+        checkMultiply(4, 2);
+        checkMultiply(7, 2);
+        checkPositiveNegative(5);
+        checkPositiveNegative(-3);
+        checkPositiveNegative(0);
     }
 
+    //------------------------------------------------------------------------------------
     //TODO ↓
     // Si el valor es menor, tiene que devolver el siguiente mensaje: X es menor que Y
     // Si el valor es mayor o igual, tiene que devolver el siguiente mensaje: X es mayor o igual que Y
     public static void checkLessOrGreaterEqual(int n1, int n2) {
-
+        if (n1 > n2 || n1 == n2 ){
+            System.out.println(n1 + " es mayor o igual que " + n2);
+        } else {
+            System.out.println(n1 + " es menor que " + n2);
+        }
     }
 
     //TODO ↓
     // Si es múltiplo, tiene que devolver el siguiente mensaje: X es múltiplo de Y
     // Si no es múltiplo, tiene que devolver el siguiente mensaje: X no es múltiplo de Y
     public static void checkMultiply(int n1, int n2) {
-
+        if (n1 % n2 == 0){
+            System.out.println(n1 + " es múltiplo de " + n2);
+        } else {
+            System.out.println(n1 + " no es múltiplo de " + n2);
+        }
     }
 
     //TODO ↓
     // Si es negativo, tiene que devolver el siguiente mensaje: X es negativo.
     // Si es positivo o cero, tiene que devolver el siguiente mensaje: X es positivo.
     public static void checkPositiveNegative(int num) {
-        
+
+        if (num < 0){
+            System.out.println(num + " es negativo.");
+        } else {
+            System.out.println(num + " es positivo.");
+        }
     }
 }
